9277407593a7dc19ee7aafbf6bde9e39c0ddcccc
Admin_2 <alexdii4000@gmail.com>
2021-08-12 12:26:39 +0300

    method toString

---
+++ src/Lesson20ToString.java
@@ -0,0 +1,19 @@
+public class Lesson20ToString {
+    public static void main(String[] args) {
+        Human1 h1 = new Human1("Bob", 45);
+        System.out.println(h1);          //  System.out.println(h1.toString) -> скрыто
+    }
+}
+
+class Human1{
+    private String name;
+    private int age;
+
+    public Human1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString() {          // переопределение метода toString, который наследуется по умолчанию
+        return name+","+age;            // из класса Object. и класс и метод toString мы не видим, но они есть
+    }
+}
